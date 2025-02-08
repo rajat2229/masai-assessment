286ae78520c1e893a5004db044afed69da92f515
rajat kumar <rajat2229.be21@chitkara.edu.in>
2025-02-08 20:10:20 +0530

    updated DynamicObjectMethods.js

---
+++ solution(6feb)/DynamicObjectMethods.js
@@ -0,0 +1,32 @@
+let sentenceBuilder = {
+    subject: "I",
+    verb: "am",
+    object: "coding",
+  
+    buildSentence: function() {
+      return this.subject && this.verb && this.object 
+        ? `${this.subject} ${this.verb} ${this.object}` 
+        : "Incomplete sentence";
+    },
+  
+    updateProperty: function(property, value) {
+      return this.hasOwnProperty(property) 
+        ? (this[property] = value, "Property updated") 
+        : "Invalid property";
+    }
+  };
+  
+  // Example Usage
+  console.log(sentenceBuilder.buildSentence()); // "I am coding"
+  
+  sentenceBuilder.updateProperty("verb", "am learning");
+  console.log(sentenceBuilder.buildSentence()); // "I am learning coding"
+  
+  sentenceBuilder.updateProperty("subject", "The cat");
+  console.log(sentenceBuilder.buildSentence()); // "The cat am learning coding"
+  
+  console.log(sentenceBuilder.updateProperty("mood", "happy")); // "Invalid property"
+  
+  sentenceBuilder.updateProperty("verb", "");
+  console.log(sentenceBuilder.buildSentence()); // "Incomplete sentence"
+  
