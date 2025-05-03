512b6b00813109b9c228983505c9c55a339c4208
rajat kumar <rajat2229.be21@chitkara.edu.in>
2025-05-03 11:02:21 +0530

    file added

---
+++ java/js/arrays/5question.js
@@ -0,0 +1,10 @@
+// 5. Splice
+// Given const arr = [1, 2, 3, 4, 5], remove 3 and insert 99, 100.
+// Return the modified array.
+
+
+const arr = [1, 2, 3, 4, 5];
+
+arr.splice(2, 1, 99, 100);
+
+console.log(arr); 
